bc6eb3cb5f1de6819a0087f695f3546aded680c8
Patrick <pa.trickjlp@gmail.com>
2021-10-20 21:37:36 +0200

    upgrade

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='arorm',
-    version='0.1.0',
+    version='0.2.0',
     description='A python orm with identity pattern mainly for arango',
     url='https://github.com/patricklx/ar-orm',
     author='Patrick Pircher',
