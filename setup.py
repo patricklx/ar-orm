06095979df462902ba3a60df6cf537863fce0e6d
patrick <pa.trickjlp@gmail.com>
2022-03-15 10:22:13 +0100

    add long description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='arorm',
-    version='0.3.0',
+    version='0.3.1',
     description='A python orm with identity pattern mainly for arango',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/patricklx/ar-orm',
     author='Patrick Pircher',
     author_email='',
